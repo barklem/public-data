de54d2f2ce6ebd3e00e7d20743a7bd461d0fd08e
Jon Grumer <jongrumer@gmail.com>
2020-09-29 13:44:00 +0200

    Tiny mods to merged_states + MN LCAO data summary
    1) changed e.g. the LiII labelling to Li+ to be able to consistently
       identify ionic states.
    2) updated the branchfrac routine to support additional LCAOdata
       subfolders
    3) compiled a LCAO data collection file for MN processes of all systems
       starting from ionic groundstates, at 1000, 3000 and 7000 K. Based on
       rates and thus assuming a maxwellian velocity distribution.

--- tools/branchfrac.py
+++ tools/branchfrac.py
@@ -6,22 +6,16 @@ import sys, os
 # for c in cases:
 #     get_rates(c, 1000, bf_cut = 0.1, mn_groundstate_only = True)
 
-# Works for:
-# C+H Ca+H Fe+H K+H Mn+H N+H O+H Rb+H Ti+H
-
-# Not yet (non-standard data format)
-# Li+H - single file
-# Mg+H - single file
-# Na+H - single file
-# Si+H - multiple files, but states included in each
+# Works for: C+H,Ca+H, Fe+H, K+H, Mn+H, N+H, O+H, Rb+H, Ti+H, Li+H, Mg+H, Na+H, Si+H
 
 def get_rates(case, temp, bf_cut = 0.0, mn_groundstate_only=False):
     """
-        get_rates(case, temp, bf_cut = 0.0, mn_groundstate_only = False)
+        get_bfs_rates(case, temp, bf_cut = 0.0, mn_groundstate_only = False)
 
-        Function to get rates and branching fractions for a certain pair of reactants at a given temperature.
+        Function to get branching fractions from rate coeffs for a certain pair of
+        reactants at a given temperature.
 
-        case                [str]     : case name, e.g. 'Ti+H'
+        case                [str]     : case name, as corresp directory: 'public-data/<case>'
         temp                [int]     : temperature in Kelvin, e.g. 1000
         bf_cut              [float]   : branching fraction cut in percent (optional, default 0.0)
         mn_groundstate_only [boolean] : include only the mutual neutralization channel from ground state (optional, default False)
@@ -29,61 +23,60 @@ def get_rates(case, temp, bf_cut = 0.0, mn_groundstate_only=False):
         returns a table printed to screen
     """
 
-    # first, check special cases (these are not implemented yet, just defining the file)
-    if case == "Si+H":
-        file_rates  = "../inelastic-"+case+'/rt_'+str(temp)+".dat"
-        file_states = ""
-        sys.exit(' case not implemented yet!')
-    elif case == "Li+H":
-        file_rates  = "../inelastic-"+case+'/lih_rates.txt'
-        file_states = ""
-        sys.exit(' case not implemented yet!')
-    elif case == "Mg+H":
-        file_rates  = "../inelastic-"+case+'/MgH_rates.txt'
-        file_states = ""
-        sys.exit(' case not implemented yet!')
-    elif case == "Na+H":
-        file_rates  = "../inelastic-"+case+'/NaH_rates.txt'
-        file_states = ""
-        sys.exit(' case not implemented yet!')
-    else:
-        # standard file structure
-        file_states = "../inelastic-"+case+'/states.dat'
-        file_rates = "../inelastic-"+case+'/'+str(temp)+"_K.rates"
+    # define (merged) states and rates files
+    file_states = "../inelastic-"+case+'/states.dat'
+    file_rates  = "../inelastic-"+case+'/'+str(temp)+"_K.rates"
+
+    # test file name definitions and update if needed
+    if not os.path.isfile(file_states):
+        # if states file doesn't exist - there's probably a separate folder with LCAO data
+        file_states = "../inelastic-"+case+'/LCAOdata/merged_states.txt'
+        file_rates  = "../inelastic-"+case+'/LCAOdata/'+str(temp)+"_K.rates"
         if not os.path.isfile(file_states):
-            print(" Error: missing states file {}!".format(file_states))
-
+            print(" Error: missing rates file {}!".format(file_states))
+    if not os.path.isfile(file_rates):
+        # if rate file isn't found then they are probaly in a separate rates folder
+        file_rates = "../inelastic-"+case+'/rates/'+str(temp)+"_K.rates"
         if not os.path.isfile(file_rates):
-            file_rates = "../inelastic-"+case+'/rates/'+str(temp)+"_K.rates"
-            if not os.path.isfile(file_rates):
-                print(" Error: missing rates file {}!".format(file_rates))
+            print(" Error: missing rates file {}!".format(file_rates))
+    if (not os.path.isfile(file_states)) or (not os.path.isfile(file_rates)):
+        return ' File(s) missing.'
 
-        if (not os.path.isfile(file_states)) or (not os.path.isfile(file_rates)):
-            return ' File(s) missing.'
-
+    # load data for selected case and temperature
     states = np.loadtxt(file_states, dtype='str')
-    rates = np.loadtxt(file_rates)
+    rates  = np.loadtxt(file_rates)
 
-    # inital states (rates columns)
+    print("\n\n  {:10} T = {:8d} K         E[cm-1]    Rate[-1]     BF[%]".format(c, int(temp)))
+
+    # inital states (columns)
     for i in range(len(states)):
         if mn_groundstate_only and '+' in states[i, -2]:
-            # final states (rates rows)
+            # final states (rows)
             sum_rates = np.sum(rates[:, i])
-            print("  {:20}".format(states[i, -2]))
+            print("\n  {:28} {:12.1f}".format(states[i, -2], float(states[i,-1])))
             for j in range(len(states)):
-                if 100*rates[j, i]/sum_rates >= bf_cut:
-                    print("  ----->  {:20} {:10} {:8.2f}".format(
-                        states[j, -2], rates[j, i], 100*rates[j, i]/sum_rates))
+                if sum_rates > 1e-40:
+                    if 100*rates[j, i]/sum_rates >= bf_cut:
+                        print("  ----->  {:20} {:12.1f} {:12.3e} {:8.2f}".format(
+                            states[j, -2], float(states[j, -1]), rates[j, i], 100*rates[j, i]/sum_rates))
             break
 
         elif not mn_groundstate_only:
             # final states (rates rows)
             sum_rates = np.sum(rates[:, i])
-            print("  {:20}".format(states[i, -2]))
+            print("\n  {:28} {:12.1f}".format(states[i, -2], float(states[i,-1])))
             for j in range(len(states)):
-                if 100*rates[j, i]/sum_rates >= bf_cut:
-                    print("  ----->  {:20} {:10} {:8.2f}".format(
-                        states[j, -2], rates[j, i], 100*rates[j, i]/sum_rates))
+                if sum_rates > 1e-40:
+                    if 100*rates[j, i]/sum_rates >= bf_cut:
+                        print("  ----->  {:20} {:12.1f} {:12.3e} {:8.2f}".format(
+                            states[j, -2], float(states[j, -1]), rates[j, i], 100*rates[j, i]/sum_rates))
         else:
             continue
 
+# Test
+cases = ["Li+H", "C+H", "N+H", "O+H", "Na+H", "Mg+H", "Si+H", "K+H", "Ca+H", "Ti+H", "Mn+H", "Fe+H", "Rb+H"]
+temps = [1000, 3000, 7000]
+
+for T in temps:
+    for c in cases:
+        get_rates(c, T, bf_cut = 0.00, mn_groundstate_only = True)
